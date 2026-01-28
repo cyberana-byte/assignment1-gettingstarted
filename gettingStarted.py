29bac796d3c2624325027d150fb5709720c7de8b
Ana <ak5538@nyu.edu>
2026-01-28 18:31:17 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -22,10 +22,10 @@ def welcome_assignment_answers(question):
         answer = "No"
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 4
+        answer = 7
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 3
 
     else:
         answer = "Error: Question not recognized."
@@ -36,6 +36,7 @@ def welcome_assignment_answers(question):
 if __name__ == "__main__":
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+
 
 
 
